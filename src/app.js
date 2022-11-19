fb5386884bfb96f90261e88b681cbb35fba65154
latifaalkhanjary <75576317+latifaalkhanjary@users.noreply.github.com>
2022-11-19 10:58:49 +0000

    Updated temperature & description

--- src/app.js
+++ src/app.js
@@ -2,7 +2,15 @@ let currentTime = new Date();
 
 let date = document.querySelector("#date-current");
 
-let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+let days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 let day = days[currentTime.getDay()];
 
 let hour = currentTime.getHours();
@@ -11,7 +19,18 @@ let minutes = currentTime.getMinutes();
 date.innerHTML = `${day} ${hour}:${minutes}`;
 
 function displayTemperature(response) {
+  console.log(response.data.main.temp);
   console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
 }
 
 let apiKey = "c0017b263128423e83fd8e6bcb99607b";
