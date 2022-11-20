9d88cc5f9c64ddd8ba4503f6b97f2bba1bd01ee3
latifaalkhanjary <75576317+latifaalkhanjary@users.noreply.github.com>
2022-11-20 16:09:42 +0000

    ✅

--- src/app.js
+++ src/app.js
@@ -66,7 +66,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "c0017b263128423e83fd8e6bcb99607b";
   let apiUrl = `  https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric
 `;
