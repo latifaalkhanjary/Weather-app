e6f187f085d629a3be6cf9bac6ea21859fa4a14a
latifaalkhanjary <75576317+latifaalkhanjary@users.noreply.github.com>
2022-11-20 13:47:27 +0000

    added forecast function

--- src/app.js
+++ src/app.js
@@ -24,8 +24,29 @@ function formatDate(timestamp) {
 
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-
-  forecastElement.innerHTML = "Forecast";
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` <div class="col-2">
+      <div class="weather-forecast-data" id="forecast">
+        ${day}
+      </div>
+      <img
+        src="http://openweathermap.org/img/wn/10d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">18°</span>
+        <span class="weather-forecast-temperature-min">12°</span>
+      </div>
+    </div>
+    `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
